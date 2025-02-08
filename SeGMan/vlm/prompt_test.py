7896173e5271155576ad858f4cd3f076725b550f
ulasdilek <ulas.dilek@ug.bilkent.edu.tr>
2025-02-08 17:21:31 +0300

    minor update to system prompt

--- SeGMan/vlm/prompt_test.py
+++ SeGMan/vlm/prompt_test.py
@@ -24,56 +24,10 @@ class VLMResponse(BaseModel):
 TASK_A2O = "agent to object"
 TASK_O2G = "object to goal"
 
-def build_prompt(task: str, agent_color, agent_pos, obj_color, obj_pos, obstacles: list, goal_pos):
-    # task: {agent to object, object to goal}
-    # grid & coordinate system
-    # agent pos
-    # object pos
-    # which one is object
-    # (obstacle identifier, obstacle pos)
-    # goal pos
-
-    prompt_dict = {
-        "task": task,
-        "agent": {
-            "color": agent_color,
-            "position": agent_pos,
-        },
-        "target object": {
-            "color": obj_color,
-            "object": obj_pos,
-        },
-        "obstacles": obstacles, # obstacle id & position
-        "goal position": goal_pos
-    }
-
+def build_prompt(prompt_dict: dict) -> str:
     puzzle_info_str = json.dumps(prompt_dict)
-    
     return puzzle_info_str
-
-
-# SYSTEM_PROMPT = """You will be provided with a top-down view of a 2D pick-and-place puzzle. The scene is surrounded by walls with endpoints at [(-2.0, -2.0), (-2.0, 2.0), (2.0, -2.0), (2.0, 2.0)] which cannot be crossed by any object within the environment. Each puzzle contains an agent, a target object, a goal position, and movable obstacles. The agent can only move in XY directions with 2-dof, and cannot rotate. The agent can pick an obstacle or the target object and move them somewhere else. The agent or any object must not collide with anything else as a rule. The agent can pick an object an move it, and can repeat these pick-and-place movements multiple times. The puzzle is solved when the target object is above the goal position.
-
-# The scene is overlaid with a grid with tick with 0.2 unit length to provide you a coordinate reference system. But nothing is constrained to the grid and positions can take decimal values.
-
-# Your task is to analyze the scene, distinguish and identify the objects, reason a path (possibly multi-step) for the agent to carry out the given task, identify obstacles that obstruct the path and propose positions for the object in the correct order as subgoals. When you provide obstacle-position pairs; the agent will attempt to move to the obstacle, pick it and place it to the proposed position, and continue with the processing of the next pair until the list is processed completely. If the given task can be solved without any manipulation to the obstacles, then leave your response empty.
-
-# The structure of your result must be a valid json in the form:
-# {
-#     task: str,
-#     puzzle_description: str,
-#     path_explanation: str,
-#     obstructing_obstacles_ids: list[str],
-#     obstacle_position_pairs: list[ObstaclePositionPair]
-# }
-
-# ObstaclePositionPair is of the form:
-# {
-#     reasoning: str,
-#     obstacle_id: str,
-#     obstacle_move_position: [float, float]
-# }
-# """
+    
 
 SYSTEM_PROMPT = """Your task is to analyze a 2D pick-and-place puzzle environment, given in a top-down view with the camera positioned on top of [0.0, 0.0] (center), and plan a sequence of actions for an agent to move an object to a goal position without causing collisions. 
 
@@ -129,15 +83,15 @@ Your result must be in JSON format:
 
 ```json
 {
-    "task": "object to goal.",
-    "puzzle_description": "A straightforward layout with one notable obstacle requiring maneuver.",
-    "path_explanation": "The target's direct path is blocked. Removing the obstacle at position X will clear the path.",
+    "task": "agent to object",
+    "puzzle_description": "The yellow agent is at the center of the puzzle. The target object is at the upper right corner surrounded by walls and obstacles. The goal position is at the lower half of the puzzle and is free from obstacles. There are two obstacles near the target object, one on the left and one on the right.",
+    "path_explanation": "The target object is blocked from left and right by obstacles, and top and bottom by walls. The agent must relocate one of the obstacles to reach the target object and then move the target object to the goal position. obstacle_1 can be moved out of the way easily to clear the path.",
     "obstructing_obstacles_ids": ["obstacle_1"],
     "obstacle_position_pairs": [
         {
-            "reasoning": "The obstacle is directly in the planned path of the target.",
+            "reasoning": "The obstacle blocks the left side of the target object.",
             "obstacle_id": "obstacle_1",
-            "obstacle_move_position": [0.0, 1.4]
+            "obstacle_move_position": [-1.1, -1.63]
         }
     ]
 }
@@ -151,7 +105,6 @@ Your result must be in JSON format:
 - The anchor of each object is its center, not its corner."""
 
 if __name__ == "__main__":
-
 
     CONFIG_NAME = "p1-two-blocks"
 
@@ -167,23 +120,14 @@ if __name__ == "__main__":
 
         C0 = ry.Config()
         C0.addFile(f"../src/config/{CONFIG_NAME}/{CONFIG_NAME}.g")
-        agent_color = (0.96875, 0.7421875, 0.30859375)
-        agent_pos = C0.getFrame("ego").getPosition()[:2].tolist()
-        obj_color = (0, 0, 1.0)
-        obj_pos = C0.getFrame("obj").getPosition()[:2].tolist()
-        obstacles = [("obj1", C0.getFrame("obj1").getPosition()[:2].tolist()),
-                    ("obj2", C0.getFrame("obj2").getPosition()[:2].tolist())]
-        goal_pos = C0.getFrame("goal").getPosition()[:2].tolist()
 
-        prompt = build_prompt(
-            task=TASK_A2O,
-            agent_color=agent_color,
-            agent_pos=agent_pos,
-            obj_color=obj_color,
-            obj_pos=obj_pos,
-            obstacles=obstacles,
-            goal_pos=goal_pos
-            )
+        prompt = build_prompt(generate_dict(
+            TASK_A2O,
+            C0,
+            agent_name="ego",
+            obj_name="obj",
+            goal_name="goal"
+        ))
         if not os.path.exists(image_path):
             overlay_image_over_config(
                 C0,
@@ -231,13 +175,14 @@ if __name__ == "__main__":
             max_completion_tokens=2000
         )
 
+        vlm_response = response.choices[0].message.parsed
+
         print(f"{CONFIG_NAME} - {i}")
         print(prompt)
         print()
-        print(response.choices[0].message.content)
+        print(json.dumps(vlm_response.model_dump(), indent=2))
         print("\n\n")
 
-        vlm_response = response.choices[0].message.parsed
         if vlm_response.obstructing_obstacles_ids:
             obstacle_ids = vlm_response.obstructing_obstacles_ids
             opp_list = vlm_response.obstacle_position_pairs
@@ -246,8 +191,8 @@ if __name__ == "__main__":
                 id = opp.obstacle_id
                 new_position = opp.obstacle_move_position
                 C0.addFrame("new_"+id)\
-                    .setShape(ry.ST.cylinder, [1.0, 0.1])\
-                    .setColor([0, 0, 0])\
+                    .setShape(ry.ST.cylinder, [2.0, 0.05])\
+                    .setColor(C0.getFrame(id).getAttributes()["color"])\
                     .setPosition([new_position[0], new_position[1], 0.1])
             
             C0.view(True)
